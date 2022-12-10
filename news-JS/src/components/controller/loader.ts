d4b3b6ad67f3e9f6303d6bff71c0c50e91239918
Aleksandr Yermolaev <sanyagt50@gmail.com>
2022-12-10 17:14:30 +0300

    refactor: add typing to news.ts

--- news-JS/src/components/controller/loader.ts
+++ news-JS/src/components/controller/loader.ts
@@ -1,40 +1,40 @@
 type sourceObject = {
-    id: string;
-    name: string;
-    description: string;
-    url: string;
-    category: string;
-    language: string;
-    country: string;
+  id: string;
+  name: string;
+  description: string;
+  url: string;
+  category: string;
+  language: string;
+  country: string;
 };
 
-type everythingObject = {
-    source: {
-        id: string;
-        name: string;
-    };
-    author: string;
-    title: string;
-    description: string;
-    url: string;
-    urlToImage: string;
-    publishedAt: string;
-    content: string;
+export type everythingObject = {
+  source: {
+    id: string;
+    name: string;
+  };
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
 };
 
 export interface EverythingData {
-    status: string;
-    totalResults: number;
-    articles: Array<everythingObject>;
+  status: string;
+  totalResults: number;
+  articles: Array<everythingObject>;
 }
 
 export interface SourceData {
-    status: string;
-    sources: Array<sourceObject>;
+  status: string;
+  sources: Array<sourceObject>;
 }
 
 type urlObject = {
-    [key: string]: string | undefined;
+  [key: string]: string | undefined;
 };
 
 type endpointType = 'sources' | 'everything';
@@ -42,55 +42,55 @@ type endpointType = 'sources' | 'everything';
 export type callbackType<T> = (d?: T) => void;
 
 class Loader {
-    baseLink: string;
-    options: { apiKey: string };
-    constructor(baseLink: string, options: { apiKey: string }) {
-        this.baseLink = baseLink;
-        this.options = options;
+  baseLink: string;
+  options: { apiKey: string };
+  constructor(baseLink: string, options: { apiKey: string }) {
+    this.baseLink = baseLink;
+    this.options = options;
+  }
+
+  public getResp(
+    { endpoint, options = {} }: { endpoint: endpointType; options?: { sources?: string } },
+    callback: callbackType<EverythingData | SourceData> = () => {
+      console.error('No callback for GET response');
+    }
+  ): void {
+    this.load('GET', endpoint, callback, options);
+  }
+
+  private errorHandler(res: Response): Response {
+    if (!res.ok) {
+      if (res.status === 401 || res.status === 404)
+        console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+      throw Error(res.statusText);
     }
 
-    public getResp(
-        { endpoint, options = {} }: { endpoint: endpointType; options?: { sources?: string } },
-        callback: callbackType<EverythingData | SourceData> = () => {
-            console.error('No callback for GET response');
-        }
-    ): void {
-        this.load('GET', endpoint, callback, options);
-    }
+    return res;
+  }
 
-    private errorHandler(res: Response): Response {
-        if (!res.ok) {
-            if (res.status === 401 || res.status === 404)
-                console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
-            throw Error(res.statusText);
-        }
+  private makeUrl(options: { sources?: string }, endpoint: endpointType): string {
+    const urlOptions: urlObject = { ...this.options, ...options };
+    let url = `${this.baseLink}${endpoint}?`;
 
-        return res;
-    }
+    Object.keys(urlOptions).forEach((key) => {
+      url += `${key}=${urlOptions[key]}&`;
+    });
 
-    private makeUrl(options: { sources?: string }, endpoint: endpointType): string {
-        const urlOptions: urlObject = { ...this.options, ...options };
-        let url = `${this.baseLink}${endpoint}?`;
+    return url.slice(0, -1);
+  }
 
-        Object.keys(urlOptions).forEach((key) => {
-            url += `${key}=${urlOptions[key]}&`;
-        });
-
-        return url.slice(0, -1);
-    }
-
-    private load(
-        method: string,
-        endpoint: endpointType,
-        callback: callbackType<EverythingData | SourceData>,
-        options: { sources?: string } = {}
-    ) {
-        fetch(this.makeUrl(options, endpoint), { method })
-            .then(this.errorHandler)
-            .then((res) => res.json())
-            .then((data: EverythingData | SourceData) => callback(data))
-            .catch((err: ErrorCallback) => console.error(err));
-    }
+  private load(
+    method: string,
+    endpoint: endpointType,
+    callback: callbackType<EverythingData | SourceData>,
+    options: { sources?: string } = {}
+  ) {
+    fetch(this.makeUrl(options, endpoint), { method })
+      .then(this.errorHandler)
+      .then((res) => res.json())
+      .then((data: EverythingData | SourceData) => callback(data))
+      .catch((err: ErrorCallback) => console.error(err));
+  }
 }
 
 export default Loader;
--- news-JS/src/components/view/news/news.ts
+++ news-JS/src/components/view/news/news.ts
@@ -1,38 +1,52 @@
+import { everythingObject } from '../../controller/loader';
 import './news.css';
 
+function nonNullQuerySelector(parentNode: ParentNode, selector: string): HTMLElement {
+  const elem = parentNode.querySelector(selector);
+  if (!elem) {
+    throw new Error('Must be an HTMLElement!');
+  }
+  if (!(elem instanceof HTMLElement)) {
+    throw new Error('Must be an HTMLElement!');
+  }
+  return elem;
+}
+
 class News {
-    draw(data) {
-        const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+  draw(data: everythingObject[]) {
+    const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
 
-        const fragment = document.createDocumentFragment();
-        const newsItemTemp = document.querySelector('#newsItemTemp');
+    const fragment = document.createDocumentFragment();
+    const newsItemTemp = document.querySelector('#newsItemTemp');
+    if (newsItemTemp instanceof HTMLTemplateElement) {
+      news.forEach((item, idx) => {
+        const newsClone = newsItemTemp.content.cloneNode(true);
 
-        news.forEach((item, idx) => {
-            const newsClone = newsItemTemp.content.cloneNode(true);
+        if (!(newsClone instanceof DocumentFragment)) {
+          throw new Error('An element is missing!');
+        }
+        if (idx % 2) nonNullQuerySelector(newsClone, '.news__item').classList.add('alt');
 
-            if (idx % 2) newsClone.querySelector('.news__item').classList.add('alt');
+        nonNullQuerySelector(newsClone, '.news__meta-photo').style.backgroundImage = `url(${
+          item.urlToImage || 'img/news_placeholder.jpg'
+        })`;
+        nonNullQuerySelector(newsClone, '.news__meta-author').textContent = item.author || item.source.name;
+        nonNullQuerySelector(newsClone, '.news__meta-date').textContent = item.publishedAt
+          .slice(0, 10)
+          .split('-')
+          .reverse()
+          .join('-');
+        nonNullQuerySelector(newsClone, '.news__description-title').textContent = item.title;
+        nonNullQuerySelector(newsClone, '.news__description-source').textContent = item.source.name;
+        nonNullQuerySelector(newsClone, '.news__description-content').textContent = item.description;
+        nonNullQuerySelector(newsClone, '.news__read-more a').setAttribute('href', item.url);
 
-            newsClone.querySelector('.news__meta-photo').style.backgroundImage = `url(${
-                item.urlToImage || 'img/news_placeholder.jpg'
-            })`;
-            newsClone.querySelector('.news__meta-author').textContent = item.author || item.source.name;
-            newsClone.querySelector('.news__meta-date').textContent = item.publishedAt
-                .slice(0, 10)
-                .split('-')
-                .reverse()
-                .join('-');
-
-            newsClone.querySelector('.news__description-title').textContent = item.title;
-            newsClone.querySelector('.news__description-source').textContent = item.source.name;
-            newsClone.querySelector('.news__description-content').textContent = item.description;
-            newsClone.querySelector('.news__read-more a').setAttribute('href', item.url);
-
-            fragment.append(newsClone);
-        });
-
-        document.querySelector('.news').innerHTML = '';
-        document.querySelector('.news').appendChild(fragment);
+        fragment.append(newsClone);
+      });
     }
+    nonNullQuerySelector(document, '.news').innerHTML = '';
+    nonNullQuerySelector(document, '.news').appendChild(fragment);
+  }
 }
 
 export default News;
